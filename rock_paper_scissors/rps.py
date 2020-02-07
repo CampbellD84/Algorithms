d352d537de2cdd65cd7275787768d3d3a8388b88
Douglas Campbell <dacampbelltbone@gmail.com>
2020-02-06 22:30:46 -0500

    Complete rock, paper, scissors problem. All tests passed.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,31 @@
 
 import sys
 
+# create a helper function
+
+
+def helper_funct(old_res, games_amt, total_res):
+    # Base Case
+    if games_amt == 0:
+        # append old result to total result of permutations
+        total_res.append(old_res)
+        return old_res
+    # recursive calls
+    helper_funct(old_res + ['rock'], games_amt-1, total_res)
+    helper_funct(old_res + ['paper'], games_amt-1, total_res)
+    helper_funct(old_res + ['scissors'], games_amt-1, total_res)
+
+
 def rock_paper_scissors(n):
-  pass 
+    # create empty list
+    result = []
+    helper_funct([], n, result)
+    return result
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
